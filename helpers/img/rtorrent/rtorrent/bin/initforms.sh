5d9e37ced03ed58e986ba8cdbad0474d62d951e2
purquapas <purkuapa2015@mail.ru>
2015-08-25 01:18:05 +0300

    test

--- helpers/img/rtorrent/rtorrent/bin/initforms.sh
+++ helpers/img/rtorrent/rtorrent/bin/initforms.sh
@@ -12,6 +12,15 @@ workdir="${cbsd_workdir}"
 
 /usr/local/bin/sqlite3 ${MYPATH}/forms.sqlite << EOF
 BEGIN TRANSACTION;
-INSERT INTO forms ( param,desc,defaults,mandatory,attr ) VALUES ( "fqdn","Please enter vhost FQDN",'',1, "maxlen=30" );
+INSERT INTO forms ( group_id,order_id,param,desc,def,cur,new,mandatory,attr,type ) VALUES ( 1,1,"fqdn","Please enter vhost FQDN",'example.my.domain','','',1, "maxlen=60", "inputbox" );
+COMMIT;
+EOF
+
+# Put version
+/usr/local/bin/cbsd ${miscdir}/updatesql ${MYPATH}/forms.sqlite /usr/local/cbsd/share/forms_version.schema
+
+/usr/local/bin/sqlite3 ${MYPATH}/forms.sqlite << EOF
+BEGIN TRANSACTION;
+INSERT INTO system ( version ) VALUES ( "201502" );
 COMMIT;
 EOF
